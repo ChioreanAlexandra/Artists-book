824846606d1e05e838f7160b51e091d9a01f220f
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-22 08:24:46 +0300

    Rename methods for saving image and file;  add comments

--- websitePages/saveImage.php
+++ websitePages/saveImage.php
@@ -1,26 +1,45 @@
 <?php
-const LOCAL_PATH='/home/alexandrachiorean/Documents/uploads_artists-book/';
-const INFO_FILE_NAME='info.txt';
-function createDirectory(string $artistName, string $email):string
+const LOCAL_PATH = '/home/alexandrachiorean/Documents/uploads_artists-book/';
+const INFO_FILE_NAME = 'info.txt';
+/**
+ * Creats a directory for each artist based on his name and email. We hash them for security;
+ * @param string $artistName
+ * @param string $email
+ * @return string
+ */
+function createDirectory(string $artistName, string $email): string
 {
-    $encodePath=sprintf('%s%s%s/',LOCAL_PATH,md5($artistName),md5($email));
-    echo "This is my encode path".$encodePath;
-    if(file_exists($encodePath))
-    {
+    $encodePath = sprintf('%s%s%s/', LOCAL_PATH, md5($artistName), md5($email));
+    echo "This is my encode path" . $encodePath;
+    if (file_exists($encodePath)) {
         return $encodePath;
     }
-    mkdir($encodePath,0777);
+    mkdir($encodePath, 0777);
     return $encodePath;
 
 }
-function saveImage(string $path, string $imageName, string $imageLocation)
+
+/**
+ * Saves the picture uploaded by artist to his local directory
+ * @param string $path
+ * @param string $imageName
+ * @param string $imageLocation
+ */
+function saveImageToDirectory(string $path, string $imageName, string $imageLocation)
 {
-    move_uploaded_file($imageLocation,$path.$imageName);
+    move_uploaded_file($imageLocation, $path . $imageName);
 }
-function saveFile(array $inputsValue,$path)
+
+/**
+ * Save the informations given by artist intro a file in his directory in JSON format.
+ *
+ * @param array $inputsValue
+ * @param $path
+ */
+function writeDetailsIntoFile(array $inputsValue, string $path)
 {
-    $contentInJSONFormat=json_encode($inputsValue);
-    $handler=fopen($path.INFO_FILE_NAME,'w');
-    fwrite($handler,$contentInJSONFormat);
+    $contentInJSONFormat = json_encode($inputsValue);
+    $handler = fopen($path . INFO_FILE_NAME, 'w');
+    fwrite($handler, $contentInJSONFormat);
 
 }
