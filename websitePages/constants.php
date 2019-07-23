7767cd5b0b4eb20bc84fe3356c30253d0888930f
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-23 17:12:05 +0300

    add constants file for image upload processing

---
+++ websitePages/constants.php
@@ -0,0 +1,27 @@
+<?php
+const IMAGE_TITLE = 'imageTitle';
+const IMAGE_DESCR = 'imageDescription';
+const ARTIST_NAME = 'artistName';
+const ARTIST_EMAIL = 'email';
+const CAMERA_SPECS = 'cameraSpecs';
+const PRICE_IMAGE = 'imagePrice';
+const DATE = 'captureDate';
+const TAG = 'tag';
+const FILE_LOCATION ='tmp_name';
+const IMAGE_FILE_NAME='name';
+const IMAGE_TAG='image';
+const LOCAL_PATH = 'uploads/';
+const INFO_FILE_NAME = 'info';
+const VALIDATE_EXTENSION_PATTERN='/\.(?<ext>\w+)/';
+const TAGS=['Adventure Photography',
+            'Astrophotography',
+            'Black and White Photography',
+            'Cityscape Photography',
+            'Creative Photography',
+             'Family Photography',
+            'Fine Art Photography',
+            'Infrared Photography',
+            'Landscape Photography',
+            'Milky Way Photography'];
+const CURRENCY='currency';
+const FILE_EXTENSION='.json';
