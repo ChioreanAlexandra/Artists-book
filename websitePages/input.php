cb637e7d2f069c7e1d83cdde41f12bc9056fe866
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-23 16:51:55 +0300

    move constants to constants.php

--- websitePages/input.php
+++ websitePages/input.php
@@ -1,17 +1,5 @@
 <?php
-const IMAGE_TITLE = 'imageTitle';
-const IMAGE_DESCR = 'imageDescription';
-const ARTIST_NAME = 'artistName';
-const ARTIST_EMAIL = 'email';
-const CAMERA_SPECS = 'cameraSpecs';
-const PRICE_IMAGE = 'imagePrice';
-const DATE = 'captureDate';
-const TAG = 'tag';
-const FILE_LOCATION ='tmp_name';
-const FILE_NAME='name';
-const IMAGE_TAG='image';
 const DATE_PATTERN='/(?<year>\d\d\d\d)-(?<month>\d\d)-(?<day>\d\d)/';
-const TAGS=['NU'];
 /**
  * Check if all the text inputs are filled
  * @return string
@@ -29,8 +17,7 @@ function getInputValues():array
     {
       $inputValues[$key]=$value;
     }
-    var_dump($_FILES);
-    $inputValues[FILE_NAME]=$_FILES[IMAGE_TAG][FILE_NAME];
+    $inputValues[IMAGE_FILE_NAME]=$_FILES[IMAGE_TAG][IMAGE_FILE_NAME];
     $inputValues[FILE_LOCATION]=$_FILES[IMAGE_TAG][FILE_LOCATION];
     return $inputValues;
 }
