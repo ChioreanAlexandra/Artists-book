1560742776dc9b8cb8f333220eca79cef91e8d7c
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-27 12:20:27 +0300

    change header location

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,29 +3,34 @@
 
 namespace MyApp\Controller;
 
+use MyApp\Model\DomainObjects\User;
+use MyApp\Model\Validation\FormValidators\LoginFormValidator;
 
 class UserController
 {
     public static function loginPage()
     {
-        require_once ("src/View/Templates/login-form.php");
-
+        require_once("src/View/Templates/login-form.php");
     }
 
     public static function login()
     {
-       // echo 'esti pe post';
-        require_once ("src/View/Templates/home-page.php");
+        header('Location:/user/profile');
 
     }
 
     public static function registerPage()
     {
-        require_once ("src/View/Templates/register-form.php");
+        require_once("src/View/Templates/register-form.php");
     }
 
     public static function register()
     {
-        require_once ("src/View/Templates/profile-page.php");
+        require_once("src/View/Templates/profile-page.php");
+    }
+
+    public static function profile()
+    {
+        require_once("src/View/Templates/profile-page.php");
     }
 }
