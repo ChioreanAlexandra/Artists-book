162fe636370dfbaf8eb81bd94b316b33c21bda01
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-27 12:23:20 +0300

    create constructor and fields

--- src/Model/Validation/FormValidators/LoginFormValidator.php
+++ src/Model/Validation/FormValidators/LoginFormValidator.php
@@ -2,7 +2,18 @@
 
 namespace MyApp\Model\Validation\FormValidators;
 
+use MyApp\Model\DomainObjects\User;
+
 class LoginFormValidator
 {
+    /** @var User */
+    private $user;
+    /** @var RulesCommand[] */
+    private $commands;
+
+    public function __construct(User $user)
+    {
+        $this->user=$user;
+    }
 
 }
