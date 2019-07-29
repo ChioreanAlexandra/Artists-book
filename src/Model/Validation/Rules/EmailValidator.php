e7ca4a32a2b7547459d24ae8403bbb7aa36b4961
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-29 17:17:47 +0300

    add email and empty validation functionality

--- src/Model/Validation/Rules/EmailValidator.php
+++ src/Model/Validation/Rules/EmailValidator.php
@@ -1,7 +1,26 @@
 <?php
 
+namespace MyApp\Model\Validation\Rules;
 
-class EmailValidator
+use MyApp\Model\DomainObjects\User;
+use MyApp\Model\Exception\EmailException;
+use \MyApp\Model\Validation\Rules\RulesCommand;
+
+class EmailValidator implements RulesCommand
 {
+    /** @var string */
+    private $email;
 
+    public function __construct(string $email)
+    {
+        $this->email=$email;
+    }
+
+    public function executeRule()
+    {
+        if(!filter_var($this->email,FILTER_VALIDATE_EMAIL))
+        {
+            //throw EmailException::emailException();
+        }
+    }
 }
--- src/Model/Validation/Rules/EmptyValidator.php
+++ src/Model/Validation/Rules/EmptyValidator.php
@@ -1,7 +1,21 @@
 <?php
 
+namespace MyApp\Model\Validation;
 
-class EmptyValidator
+use MyApp\Model\Validation\Rules\RulesCommand;
+
+class EmptyValidator implements RulesCommand
 {
+    /** @var array */
+    private $inputValues;
 
+    public function executeRule(): bool
+    {
+        foreach($this->inputValues as $item)
+        {
+            if(empty($item))
+                return false;
+        }
+        return true;
+    }
 }
