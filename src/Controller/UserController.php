df5fb01a52aa3406a055e6526794f7cc23e94d11
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-29 17:11:17 +0300

    call rendere methods and add functionlaity for login method

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,25 +3,69 @@
 
 namespace MyApp\Controller;
 
+
 use MyApp\Model\DomainObjects\User;
+use MyApp\Model\FormMapper\LoginFormMapper;
+use MyApp\Model\Helper\Form\UserField;
+use MyApp\Model\Http\Session;
 use MyApp\Model\Validation\FormValidators\LoginFormValidator;
+use MyApp\Model\Http\Request;
+use MyApp\View\Renderers\LoginRenderer;
+use MyApp\View\Renderers\ProfilePageRenderer;
+use MyApp\View\Renderers\RegisterRenderer;
+
 
 class UserController
 {
+    /** @var array */
+    public static $error;
+
     public static function loginPage()
     {
-        require_once("src/View/Templates/login-form.php");
+        LoginRenderer::render();
+    }
+
+    public static function getRequest():Request
+    {
+        return new Request();
+    }
+
+    public static function getSession():Session
+    {
+        return new Session();
     }
 
     public static function login()
     {
+        $password='1234';
+        $email='ale@yahoo.com';
+        $request=self::getRequest();
+        $error=[];
+        $loginFormMapper=new LoginFormMapper($request);
+        $loginUser=$loginFormMapper->createUserFromLoginForm();
+        echo $loginUser;
+        if($password != $loginUser->getPassword())
+        {
+            $error[UserField::getPasswordField()]='Wrong password';
+        }
+        if($email != $loginUser->getEmail())
+        {
+            $error[UserField::getEmailField()]='Wrong email';
+        }
+
+        if(!empty($error)) {
+            LoginRenderer::render($error);
+            return;
+        }
+        $session=self::getSession();
+        $session->setSessionValue(UserField::getId(),11);
         header('Location:/user/profile');
 
     }
 
     public static function registerPage()
     {
-        require_once("src/View/Templates/register-form.php");
+        RegisterRenderer::render();
     }
 
     public static function register()
@@ -31,6 +75,7 @@ class UserController
 
     public static function profile()
     {
-        require_once("src/View/Templates/profile-page.php");
+        ProfilePageRenderer::render();
+        //require_once("src/View/Templates/profile-page.php");
     }
 }
