bf49f4ec8c64ec7b8f62465a55fead0b10bf415d
Alexandra Chiorean <alexandrachiorean98@gmail.com>
2019-07-29 22:30:54 +0300

    add register form mapper class

---
+++ src/Model/FormMapper/RegisterFormMapper.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace MyApp\Model\FormMapper;
+
+
+use MyApp\Model\DomainObjects\User;
+use MyApp\Model\Helper\Form\UserField;
+use MyApp\Model\Http\Request;
+
+class RegisterFormMapper
+{
+    /** @var Request  */
+    private $request;
+    public function __construct(Request $request)
+    {
+        $this->request=$request;
+    }
+
+    public function createUserFromRegisterForm():?User
+    {
+        if($this->request->getPost())
+        {
+            return new User($this->request->getPost()[UserField::getEmailField()],$this->request->getPost()[UserField::getPasswordField()],$this->request->getPost()[UserField::getNameField()]);
+        }
+        return null;
+    }
+}
