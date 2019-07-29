f58cbd32de1dc8ec0fb865d45a780a538d124f5e
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-29 17:24:31 +0300

    add Login validator class

--- src/Model/Validation/FormValidators/LoginFormValidator.php
+++ src/Model/Validation/FormValidators/LoginFormValidator.php
@@ -2,18 +2,60 @@
 
 namespace MyApp\Model\Validation\FormValidators;
 
-use MyApp\Model\DomainObjects\User;
+use MyApp\Model\Exception\EmailException;
+use MyApp\Model\Exception\EmptyFieldException;
+use MyApp\Model\Validation\Rules\EmailValidator;
+use MyApp\Model\Validation\Rules\RulesCommand;
 
 class LoginFormValidator
 {
-    /** @var User */
-    private $user;
+    /** @var array */
+    private $userDetails;
     /** @var RulesCommand[] */
     private $commands;
+    private const EMAIL='username';
 
-    public function __construct(User $user)
+    public function __construct(array $userDetails)
     {
-        $this->user=$user;
+        $this->userDetails=$userDetails;
+    }
+
+    public function addToCommandsList(RulesCommand $command)
+    {
+        $this->commands[]=$command;
+    }
+
+    private function runCommands()
+    {
+        foreach ($this->commands as $rule)
+        {
+            $rule->executeRule();
+        }
+    }
+    public function validate():array
+    {
+        $errors=[];
+        try
+        {
+            $emailValidator=new EmailValidator($this->userDetails[self::EMAIL]);
+            $this->addToCommandsList($emailValidator);
+            $emptyValidator=new EmailValidator($this->userDetails);
+            $this->addToCommandsList($emptyValidator);
+            $this->runCommands();
+        }
+        catch (EmptyFieldException $exception)
+        {
+            $errors[]=$exception->getMessage();
+        }
+        catch (EmailException $exception)
+        {
+            $errors[]=$exception->getMessage();
+        }
+        finally
+        {
+            return $errors;
+        }
+
     }
 
 }
