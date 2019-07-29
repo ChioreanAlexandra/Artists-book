12aeef4927d82643dd2e5f341bcc56e3a33a8311
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-29 17:21:46 +0300

    change namespace

--- src/Model/Http/Session.php
+++ src/Model/Http/Session.php
@@ -1,10 +1,24 @@
 <?php
 
 
-namespace MyApp\Model\Request;
+namespace MyApp\Model\Http;
 
 
 class Session
 {
+    public function getSession(): array
+    {
+        return $_SESSION;
+    }
+
+    public function getSessionValue(string $key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    public function setSessionValue(string $key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
 
 }
