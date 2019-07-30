d691ae1b938a801d8e8e435f925e2f1aa48f77cb
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-30 19:02:52 +0300

    remove findById method and findByEmail method

--- src/Model/Persistence/Finder/UserFinder.php
+++ src/Model/Persistence/Finder/UserFinder.php
@@ -1,26 +1,31 @@
 <?php
 
+namespace MyApp\Model\Persistence\Finder;
 
-use Model\DomainObject\User;
+use MyApp\Model\DomainObjects\User;
+use MyApp\Model\Helper\Form\UserField;
+use PDO;
 
 class UserFinder extends AbstractFinder
 {
-    public function findById(int $id): User
-    {
-        // TODO: you can extract the table name in a constant, or a getter function, or config
-        $sql = "select * from users where id=?";
-        $statement = $this->getPdo()->prepare($sql);
-        $statement->bindValue(0, $id, PDO::PARAM_INT);
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-        //TODO: we are assuming here the property names and column names are the same (adjust if not in your case)
-        return $this->translateToUser($row);
-    }
-
     private function translateToUser(array $row): User
     {
-        $user = new User($row['name'], $row['email'], null, $row['id']);
-
+        $user = new User($row[UserField::getEmailField()], null, $row[UserField::getNameField()], $row[UserField::getId()]);
         return $user;
+    }
+
+    public function findByCredentials(string $email, string $password): ?User
+    {
+
+        // TODO: you can extract the table name in a constant, or a getter function, or config
+        $sql = "select * from user where email=?";
+        $statement = $this->getPdo()->prepare($sql);
+        $statement->bindValue(1, $email, PDO::PARAM_STR_CHAR);
+        $statement->execute();
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        if (!$row || !password_verify($password,$row[UserField::getPasswordField()])) {
+            return null;
+        }
+        return $this->translateToUser($row);
     }
 }
