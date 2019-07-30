24c7a195de0a9c7f99cb8264b34167db4f97c898
Alexandra Chiorean <alexandrachiorean98@gmail.com>
2019-07-31 00:58:31 +0300

    delete old routing methods and call Router object method

--- index.php
+++ index.php
@@ -5,30 +5,6 @@ const CLASSNAME_REGEX='/\/(?<className>[a-z]+)\//';
 const METHODNAME_REGEX='/[a-z]+\/(?<methodName>[a-zA-Z]+)/';
 
 require_once "vendor/autoload.php";
-
-function getClass(string $serverUri):string
-{
-    preg_match(CLASSNAME_REGEX,$serverUri,$match);
-    if(isset($match['className']))
-    {
-        return ucfirst($match['className']);
-    }
-    return '';
-}
-function getMethod(string $serverUri):string
-{
-    preg_match(METHODNAME_REGEX,$serverUri,$match);
-    if(isset($match['methodName']))
-    {
-        return $match['methodName'];
-    }
-    return '';
-}
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        call_user_func('MyApp\Controller\ProductController::showProducts');
-        break;
-    default:$className=getClass($_SERVER['REQUEST_URI']);
-            $methodName=getMethod($_SERVER['REQUEST_URI']);
-            call_user_func(sprintf('%s\%s%s::%s',NAMESPACE_CONST,$className,CONTROLLER,$methodName)); break;
-}
+//session_start();
+$router=new \MyApp\Model\Router\Router($_SERVER['REQUEST_URI']);
+$router->getRoute();
