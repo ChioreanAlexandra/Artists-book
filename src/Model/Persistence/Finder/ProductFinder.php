0d7b5a494b4e4faaa630c3f8ee77c61af0158a55
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-30 19:16:00 +0300

    add class for product find methods

---
+++ src/Model/Persistence/Finder/ProductFinder.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace MyApp\Model\Persistence\Finder;
+
+
+use MyApp\Model\DomainObjects\Product;
+use MyApp\Model\Helper\Form\ProductField;
+use PDO;
+
+class ProductFinder extends AbstractFinder
+{
+    private function translateToProduct(array $row): Product
+    {
+        $product = new Product($row[ProductField::getUserIdField()], $row[ProductField::getImageTitleField()],
+                            $row[ProductField::getImageDescriptionField()], $row[ProductField::getCameraSpecsField()],
+                            new \DateTime($row[ProductField::getCaptureDate()]),$row[ProductField::getThumbnailField()],
+                            null, $row[ProductField::getId()]);
+        return $product;
+    }
+
+    public function findAll(): array
+    {
+        // TODO: you can extract the table name in a constant, or a getter function, or config
+        $sql = "select * from product ";
+        $statement = $this->getPdo()->prepare($sql);
+        $statement->execute();
+        $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $listOfProducts=[];
+        foreach($row as $product)
+        {
+            $listOfProducts[]=$this->translateToProduct($product);
+        }
+       return $listOfProducts;
+    }
+}
