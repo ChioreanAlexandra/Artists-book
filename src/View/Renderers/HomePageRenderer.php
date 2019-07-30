8132a70b341e7ad1dd1317cd07496f0be3fb863a
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-30 18:59:20 +0300

    change render method parameter name

--- src/View/Renderers/HomePageRenderer.php
+++ src/View/Renderers/HomePageRenderer.php
@@ -8,8 +8,9 @@ class HomePageRenderer
 {
     private const HOME_PAGE_HTML_FILE='src/View/Templates/home-page.php';
 
-    public static function render(array $error = [])
+    public static function render(array $productList = [])
     {
+        echo "this is product list";
         require_once "".self::HOME_PAGE_HTML_FILE."";
     }
 }
