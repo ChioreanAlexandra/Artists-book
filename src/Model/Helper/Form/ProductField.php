d387d577544cb2e4f8db83f02ca0ee1aa30055c7
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-30 18:54:21 +0300

    add class for product's constants

--- src/Model/Helper/Form/ProductField.php
+++ src/Model/Helper/Form/ProductField.php
@@ -6,12 +6,15 @@ namespace MyApp\Model\Helper\Form;
 
 class ProductField
 {
-    private const IMAGE_TITLE = 'imageTitle';
-    private const IMAGE_DESCR = 'imageDescription';
-    private const CAMERA_SPECS = 'cameraSpecs';
-    private const PRICE_IMAGE = 'imagePrice';
-    private const CAPTURE_DATE = 'captureDate';
+    private const IMAGE_TITLE = 'title';
+    private const IMAGE_DESCR = 'description';
+    private const CAMERA_SPECS = 'camera_specs';
+    private const PRICE_IMAGE = 'price';
+    private const CAPTURE_DATE = 'capture_data';
     private const TAG = 'tag';
+    private const THUMBNAIL_PATH='thumbnail_path';
+    private const USER_ID='user_id';
+    private const ID='id';
 
     public static function getImageTitleField(): string
     {
@@ -37,5 +40,18 @@ class ProductField
     {
         return self::TAG;
     }
+    public static function getThumbnailField(): string
+    {
+        return self::THUMBNAIL_PATH;
+    }
+    public static function getUserIdField(): string
+    {
+        return self::USER_ID;
+    }
+    public static function getId(): string
+    {
+        return self::ID;
+    }
+
 
 }
