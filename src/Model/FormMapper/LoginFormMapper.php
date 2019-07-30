2d92d9b14157cac76995bf84264423e9de8ded7e
Alexandra Chiorean <alexandrachiorean98@gmail.com>
2019-07-31 01:10:11 +0300

    change methods call in constructor

--- src/Model/FormMapper/LoginFormMapper.php
+++ src/Model/FormMapper/LoginFormMapper.php
@@ -19,7 +19,7 @@ class LoginFormMapper
     {
         if($this->request->getPost())
         {
-            return new User($this->request->getPost()[getEmailField()], $this->request->getPost()[getPasswordField()], CRYPT_SHA512);
+            return new User($this->request->getPost()[UserField::getEmailField()], $this->request->getPost()[UserField::getPasswordField()]);
         }
         return null;
     }
