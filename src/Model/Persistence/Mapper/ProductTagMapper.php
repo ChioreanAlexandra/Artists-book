2eb26ecbffe6e5b19181919c6fcbf30d84562676
Alexandra Chiorean <alexandrachiorean98@gmail.com>
2019-07-31 01:04:46 +0300

    add translate to array method

--- src/Model/Persistence/Mapper/ProductTagMapper.php
+++ src/Model/Persistence/Mapper/ProductTagMapper.php
@@ -4,7 +4,21 @@
 namespace MyApp\Model\Persistence\Mapper;
 
 
+use MyApp\Model\DomainObjects\ProductTag;
+
+
 class ProductTagMapper
 {
+    public function save(ProductTag $product)
+    {
+        $this->insert($product);
+    }
 
+    private function translateToArray(ProductTag $productTag): array
+    {
+        return [
+            'product_id' => $productTag->getProductId(),
+            'tag_id' => $productTag->getTagId()];
+    }
+    
 }
