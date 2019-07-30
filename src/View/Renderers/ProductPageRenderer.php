1eb987e99d3383e93329346429d63c4361b4ab7e
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-30 19:03:47 +0300

    change constant path

--- src/View/Renderers/ProductPageRenderer.php
+++ src/View/Renderers/ProductPageRenderer.php
@@ -6,7 +6,7 @@ namespace MyApp\View\Renderers;
 
 class ProductPageRenderer
 {
-    private const PRODUCT_PAGE_HTML_FILE = 'src/View/Templates/register-form.php';
+    private const PRODUCT_PAGE_HTML_FILE = 'src/View/Templates/product-page.php';
 
     public static function render(array $error = [])
     {
