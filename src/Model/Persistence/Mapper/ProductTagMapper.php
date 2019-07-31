bbe115a2678b0469820f6d953e945b7f6f6a80f2
ChioreanAlexandra <alexandra.chiorean@evozon.com>
2019-07-31 18:35:21 +0300

    add insert methods for ProductTag and OrderItem

--- src/Model/Persistence/Mapper/ProductTagMapper.php
+++ src/Model/Persistence/Mapper/ProductTagMapper.php
@@ -7,11 +7,11 @@ namespace MyApp\Model\Persistence\Mapper;
 use MyApp\Model\DomainObjects\ProductTag;
 
 
-class ProductTagMapper
+class ProductTagMapper extends AbstractMapper
 {
-    public function save(ProductTag $product)
+    public function save(int $productId, string $tagName)
     {
-        $this->insert($product);
+        $this->insert($productId, $tagName);
     }
 
     private function translateToArray(ProductTag $productTag): array
@@ -19,6 +19,16 @@ class ProductTagMapper
         return [
             'product_id' => $productTag->getProductId(),
             'tag_id' => $productTag->getTagId()];
+    }
+    private function insert(int $productId, string $tagName)
+    {
+        //TODO: transform user to array row then prepare an INSERT ($this->getPdo()) and execute
+       // $row = $this->translateToArray($tier);
+        $sql = "insert into product_tag(product_id,tag_id) values(:product_id, (select id FROM tag WHERE tag_name=:tag_name));";
+        $statement = $this->getPdo()->prepare($sql);
+        $statement->bindValue('product_id',$productId);
+        $statement->bindValue('tag_name',$tagName);
+        $statement->execute();
     }
     
 }
